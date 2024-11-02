75e57cdc92d3ee45e90eac3d7b4f47b11bdc0565
locnht-it <locnht.it@gmail.com>
2024-11-02 19:37:44 +0700

    data types

--- Program.cs
+++ Program.cs
@@ -9,3 +9,10 @@ Console.WriteLine("My age is " + age);
 
 var country = "Viet Nam";
 Console.WriteLine("My country is " + country);
+
+// #3. Data Types
+string university = "FPT University";
+
+long elonMuskNetWorth = 1000000000000000;
+
+var charchar = 'c';
