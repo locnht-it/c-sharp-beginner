71585fc1ae516ce5ba9d78337390688dc45ba697
locnht-it <locnht.it@gmail.com>
2024-11-04 20:11:41 +0700

    tuple

--- Program.cs
+++ Program.cs
@@ -257,3 +257,29 @@ RealEstate elmStreet = new()
 };
 
 Console.WriteLine(elmStreet.CalculatePricePerSquareFoot());
+
+// #13. Tuple
+Console.WriteLine("#############");
+Console.WriteLine("#13. Tuple");
+
+(string, int, string) values = ("t", 2, "p");
+
+Console.WriteLine(values.Item1);
+Console.WriteLine(values.Item2);
+Console.WriteLine(values.Item3);
+
+var valuesWithName = (First: "t", Second: 2, Third: "p");
+
+Console.WriteLine(valuesWithName.First);
+Console.WriteLine(valuesWithName.Second);
+Console.WriteLine(valuesWithName.Third);
+
+(int a, string b, bool c) ReturnTheseValues()
+{
+    return (9, "u", true);
+}
+
+(int a, string b, bool c) = ReturnTheseValues();
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
